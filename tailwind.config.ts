06890e9dc769c97e8d2b246a5819d75b0cef1516
KaanCesur354 <kaancesur354@hotmail.com>
2025-05-20 22:39:36 +0300

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,26 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    darkMode: ['class'],
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-  	extend: {
-  		colors: {
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		}
-  	}
+    extend: {
+      colors: {
+        sidebar: {
+          DEFAULT: 'hsl(var(--sidebar-background))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          primary: 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          accent: 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))'
+        },
+        
+        destructive: 'hsl(var(--destructive))',
+        'destructive-foreground': 'hsl(var(--destructive-foreground))',
+      },
+    },
   },
   plugins: [],
 }
